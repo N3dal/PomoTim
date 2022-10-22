0d9bd4532002ee801b3c13ee7b403123173206d8
N3dal <nedalxzo@gmail.com>
2022-10-22 20:26:45 +0200

    create main function

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------------
+# simple pomodoro app using tkinter;
+# this is one of my 50 python project challenge;
+#
+#
+# Author:N84.
+#
+# Create Date:Sat Oct 22 20:24:56 2022.
+# ///
+# ///
+# ///
+# -----------------------------------------------------------------
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
