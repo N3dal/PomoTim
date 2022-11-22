0c67dc9fbccbe090838ade58e2175a6de97baeb2
N3dal <nedalxzo@gmail.com>
2022-11-22 17:26:32 +0200

    update main .py

--- pyqt/main.py
+++ pyqt/main.py
@@ -44,12 +44,20 @@ class TimerLabel(QLabel):
         self.timer = QTimer()
         self.timer.timeout.connect(self.__tick)
 
+        # setup blink timer;
+        self.__blink_timer = QTimer()
+        self.__blink_timer.timeout.connect(self.__blink)
+
+        self.timer_status = False
+
     def set_time(self, time: str):
         """
             update the time on the label;
 
             return None;
         """
+
+        self.setText(time)
 
         return None
 
@@ -65,7 +73,8 @@ class TimerLabel(QLabel):
     def __format_time(self):
         pass
 
-    def __timer_value_to_seconds(self):
+    @staticmethod
+    def __timer_value_to_seconds(timer_value: str):
         """
             convert the time value in this form=> "xx:xx",
             to seconds;
@@ -73,7 +82,16 @@ class TimerLabel(QLabel):
             return int;
         """
 
-    def __seconds_to_timer_value(self):
+        minutes, seconds = timer_value.split(":")
+
+        try:
+            return int(minutes) * 60 + int(seconds)
+
+        except ValueError:
+            raise Exception("Can't convert minutes or seconds to integers!!!")
+
+    @staticmethod
+    def __seconds_to_timer_value(seconds: int):
         """
             convert the time in seconds into timer label format,
             "xx:xx";
@@ -81,24 +99,77 @@ class TimerLabel(QLabel):
             return string;
         """
 
+        minutes, seconds = divmod(seconds, 60)
+
+        return f"{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+
     def __tick(self):
         """
             Start the timer ticking;
+            remove one second in every tick;
 
             return None
         """
 
+        # guard condition;
+        if not self.timer_status:
+            return None
+
+        time_in_seconds = self.__timer_value_to_seconds(self.get_time())
+
+        time_in_seconds -= 1
+
+        if time_in_seconds < 0:
+            # stop the timer if finish;
+            self.timer_status = False
+            # set the timer to the start point;
+            self.set_time("01:00")
+
+            self.__blink_timer.start(500)
+
+            return None
+
+        new_timer_label_value = self.__seconds_to_timer_value(time_in_seconds)
+
+        self.set_time(new_timer_label_value)
+
+        return None
+
     def __blink(self):
         """"""
-        pass
+
+        font_color = self.palette().text().color().name()
+
+        if font_color == Defaults.LABEL_FG_BASE:
+            self.setStyleSheet(f"""
+                            color: {Defaults.LABEL_BLINKING_FG};
+                            font-size: 49px;
+                            font-family: {Defaults.WIN_FONT_FAMILY};
+                            font-weight: bold;
+                           """)
+
+        if font_color == Defaults.LABEL_BLINKING_FG:
+            self.setStyleSheet(f"""
+                            color: {Defaults.LABEL_FG_BASE};
+                            font-size: 49px;
+                            font-family: {Defaults.WIN_FONT_FAMILY};
+                            font-weight: bold;
+                           """)
+
+        return None
 
     def start(self):
         """"""
-        pass
+        self.timer_status = True
+        self.timer.start(10)  # time in ms;
+
+        return None
 
     def stop(self):
         """"""
-        pass
+        self.timer_status = False
+
+        return None
 
 
 class MainWindow(QMainWindow):
@@ -117,12 +188,14 @@ class MainWindow(QMainWindow):
                            font-family: {Defaults.WIN_FONT_FAMILY};
                            """)
 
-        self.timer_label = TimerLabel(parent=self, text="00:00")
+        self.timer_label = TimerLabel(parent=self, text="01:00")
 
         self.timer_label.move(
             (Defaults.WIN_WIDTH - self.timer_label.width()) // 2 + 11,  # center on x;
             (Defaults.WIN_HEIGHT - self.timer_label.height())//2  # center on y;
         )
+
+        self.timer_label.start()
 
 
 def main():
